659472c3279e6d2d94c79029f537d7e81694d1c9
Ricardo Quesada <ricardoquesada@gmail.com>
2016-02-19 21:19:07 -0800

    order in includes matter. fixes it

--- configs/chipmunk/gen_chipmunk_bridgesupport.sh
+++ configs/chipmunk/gen_chipmunk_bridgesupport.sh
@@ -4,4 +4,4 @@
 # cd cocos2d-iphone/externals/Chipmunk/include/chipmunk
 #
 # order is important
-gen_bridge_metadata -F complete --no-64-bit -c '-DCP_ALLOW_PRIVATE_ACCESS=1 -DNDEBUG -I.' cpBody.h cpSpace.h *.h -o ~/progs/jsbindings/configs/chipmunk/chipmunk.bridgesupport
+gen_bridge_metadata -F complete --no-64-bit -c '-DCP_ALLOW_PRIVATE_ACCESS=0 -DNDEBUG -I.' chipmunk_types.h cpVect.h chipmunk.h cp*Shape.h cpBody.h cpSpace.h cpTransform.h cpArbiter.h cpBB.h cpConstraint.h cp*Joint.h cp*Spring.h cp*Motor.h cpRobust.h cpPolyline.h -o ~/progs/jsbindings/configs/chipmunk/chipmunk.bridgesupport
